a00328ac6fdb365e4ba105d5be7c60eb1dfeb57d
Chris Kelly <ckdake@ckdake.com>
2014-10-13 23:12:56 -0400

    Adds some clients

--- index.php
+++ index.php
@@ -36,9 +36,9 @@
       <ul id="menu">
         <li><a href="/hosting/services.html">services</a></li>
   	    <li><a href="/hosting/platform.html">platform</a></li>
+        <li><a href="/hosting/clients.html">clients</a></li>
         <li><a href="/hosting/testimonials.html">testimonials</a></li>
   	    <li><a href="/hosting/support.html">support</a></li>
-  	    <li><a href="/hosting/payment.html">payment</a></li>
         <li><a href="/hosting/signup.html">signup</a></li>
       </ul>
     </div>
